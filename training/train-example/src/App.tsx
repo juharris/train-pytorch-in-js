daa5e264589744bfdf40f837096baa6fab07950b
Justin D. Harris <juharris@users.noreply.github.com>
2022-03-17 13:55:43 -0400

    Load the model just once.

--- training/train-example/src/App.tsx
+++ training/train-example/src/App.tsx
@@ -22,64 +22,67 @@ function App() {
 		setStatusMessage(message)
 	}
 
-	function addMessage(message: string) {
-		console.log(message)
-		console.debug("messages:", messages)
-		setMessages([...messages, message])
-	}
-
 	function showErrorMessage(message: string) {
-		console.error(message)
 		setErrorMessage(message)
 	}
 
 	// Load the ONNX model.
-	React.useMemo(async () => {
-		const url = '/gradient_graph.onnx'
-		showStatusMessage(`Loading ONNX model at "${url}".`)
-		let session
-		try {
-			session = await ort.InferenceSession.create(url)
-			console.log("Loaded the model. session:", session)
-			showStatusMessage("Loaded the model.")
-		} catch (err) {
-			showErrorMessage("Error loading the model: " + err)
-			console.error("Error loading the model", err)
-			return
+	React.useEffect(() => {
+		setMessages([])
+
+		async function loadModel() {
+			const url = '/gradient_graph.onnx'
+			showStatusMessage(`Loading ONNX model at "${url}"...`)
+			let session
+			try {
+				session = await ort.InferenceSession.create(url)
+				console.log("Loaded the model. session:", session)
+				showStatusMessage(`Loaded the model at "${url}."`)
+			} catch (err) {
+				showErrorMessage("Error loading the model: " + err)
+				console.error("Error loading the model", err)
+				return
+			}
+
+			// Set up some sample data to try with our model.
+			const x = Float32Array.from(Array.from(Array(10)).map(Math.random))
+			const label = BigInt64Array.from([1n])
+			const batchSize = 1
+			const xTensor = new ort.Tensor('float32', x, [batchSize, x.length])
+			const labelTensor = new ort.Tensor('int64', label, [batchSize])
+
+			const feeds = {
+				input: xTensor,
+				labels: labelTensor,
+				'fc1.weight': randomTensor([5, 10]),
+				'fc1.bias': randomTensor([5]),
+				'fc2.weight': randomTensor([2, 5]),
+				'fc2.bias': randomTensor([2]),
+			}
+
+			const results = await session.run(feeds)
+			console.debug("results:", results)
+			const newMessages: string[] = []
+			for (const [k, tensor] of Object.entries(results)) {
+				console.debug(k, tensor)
+				newMessages.push(`${k}: ${(tensor as any).data}`)
+			}
+			setMessages(messages => [...messages, ...newMessages])
 		}
 
-		// Set up some sample data to try with our model.
-		const x = Float32Array.from(Array.from(Array(10)).map(Math.random))
-		const label = BigInt64Array.from([1n])
-		const batchSize = 1
-		const xTensor = new ort.Tensor('float32', x, [batchSize, x.length])
-		const labelTensor = new ort.Tensor('int64', label, [batchSize])
-
-		const feeds = {
-			input: xTensor,
-			labels: labelTensor,
-			'fc1.weight': randomTensor([5, 10]),
-			'fc1.bias': randomTensor([5]),
-			'fc2.weight': randomTensor([2, 5]),
-			'fc2.bias': randomTensor([2]),
-		}
-
-		const results = await session.run(feeds)
-		console.debug("results:", results)
-		for (const [k, tensor] of Object.entries(results)) {
-			console.debug(k, tensor)
-			addMessage(`${k}: ${(tensor as any).data}`)
-		}
+		loadModel()
 	}, [])
 
 	return (<div className="App">
-		Status: {statusMessage ? <p>{statusMessage}</p> : null}
-		{messages ?
-			<ul>
-				{messages.map((m, i) =>
-					<li key={i}>{m}</li>)
-				}
-			</ul> : null}
+		<p>{statusMessage}</p>
+		{messages.length > 0 ?
+			<div>
+				<ul>
+					{messages.map((m, i) =>
+						<li key={i}>{m}</li>)
+					}
+				</ul>
+			</div> : null}
 		{errorMessage ?
 			<p style={{ color: 'red' }}>
 				{errorMessage}
