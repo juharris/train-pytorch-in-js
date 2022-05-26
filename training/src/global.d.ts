6d7a84fd0dc1b1ebfa6813439e42b7f825194c2b
Justin D. Harris <juharris@users.noreply.github.com>
2022-05-26 14:09:42 -0400

    Get ort imports working nicely.

--- training/src/global.d.ts
+++ training/src/global.d.ts
@@ -1,5 +1,14 @@
-// FIXME Try to get TypeScript to actually recognize and assist with the documentation for these types.
+// Re-export some stuff to help with with writing TypeScript code.
+// I tried this so many ways and this seems like the most concise way.
+// This is hard to maintain but it's the only way that I could figure out to get it working.
 declare module ort {
-    export { InferenceSession, Tensor } from './ort-decl'
+    export declare namespace InferenceSession {
+        export type OnnxValueMapType = import('./ort-decl/inference-session').InferenceSession.OnnxValueMapType
+        export type ReturnType = import('./ort-decl/inference-session').InferenceSession.ReturnType
+    }
+    export const InferenceSession: import('./ort-decl/inference-session').InferenceSessionFactory
+    export type InferenceSession = import('./ort-decl/inference-session').InferenceSession
+   
+    export const Tensor: import('./ort-decl/tensor').TensorConstructor
+    export type Tensor = import('./ort-decl/tensor').Tensor
 }
-
