44449a94635232c176d926b9651f785f0c376b99
Justin D. Harris <juharris@users.noreply.github.com>
2022-06-27 14:27:20 -0400

    document issues with exporting MNIST conv model.

--- export/mnist/model.py
+++ export/mnist/model.py
@@ -8,15 +8,20 @@ NUM_CLASSES=10
 
 
 # This model had issues exporting to an ONNX file.
+# "WARNING: The shape inference of org.pytorch.aten::ATen type is missing, so it may result in wrong shape inference for the exported graph. Please consider adding it in symbolic function."
+# "Warning: Checker does not support models with experimental ops: ATen"
+# "RuntimeError: /onnxruntime_src/orttraining/orttraining/python/orttraining_pybind_state.cc:789 onnxruntime::python::addObjectMethodsForTraining(pybind11::module&, onnxruntime::python::ExecutionProviderRegistrationFn)::<lambda(const pybind11::bytes&, const std::unordered_set<std::basic_string<char> >&, const std::unordered_set<std::basic_string<char> >&, std::string)> [ONNXRuntimeError] : 1 : FAIL : Node (ATen_8) output arg (19) type inference failed"
 class MnistConvNet(nn.Module):
-    def __init__(self, num_classes=NUM_CLASSES):
+    def __init__(self, is_export_mode=False, num_classes=NUM_CLASSES):
         super(MnistConvNet, self).__init__()
+        self.is_export_mode = is_export_mode
         # We did have padding='valid' but ONNX doesn't support that.
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
-        self.dropout1 = nn.Dropout(0.25)
+        if not is_export_mode:
+            self.dropout1 = nn.Dropout(0.25)
+            self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(32*32*3*3, 128)
-        self.dropout2 = nn.Dropout(0.5)
         self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor):
@@ -25,11 +30,13 @@ class MnistConvNet(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
+        if not self.is_export_mode:
+            x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
-        x = self.dropout2(x)
+        if not self.is_export_mode:
+            x = self.dropout2(x)
         x = self.fc2(x)
         output = F.softmax(x, dim=1)
         return output
